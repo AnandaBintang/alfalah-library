508c78e9a08d9a44173429dda909af62ac44503b
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2025-06-04 23:58:23 +0700

    FEAT: add Book model for managing book records

---
+++ app/Models/Book.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Book extends Model
+{
+    use HasFactory,SoftDeletes;
+
+    protected $fillable = [
+        'title',
+        'subtitle',
+        'isbn',
+        'publisher_id',
+        'stock',
+        'type',
+        'is_student_work',
+        'source',
+        'catalog_code',
+        'publication_year',
+        'classification_code',
+        'rack_location',
+        'subject',
+        'abstract',
+        'cover_image_path',
+    ];
+
+    protected $casts = [
+        'is_student_work' => 'boolean',
+    ];
+
+    public function publisher()
+    {
+        return $this->belongsTo(Publisher::class);
+    }
+
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class);
+    }
+
+    public function files()
+    {
+        return $this->hasMany(BookFile::class);
+    }
+
+    public function bookStats()
+    {
+        return $this->hasMany(BookLoanStats::class);
+    }
+
+    public function loans()
+    {
+        return $this->hasMany(Loan::class);
+    }
+}
