e057dc1f4adcc39eb67ba7c8db5dc210b96c8ba2
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2025-06-04 23:57:45 +0700

    FEAT: add ReturnedChart widget for book return activity

---
+++ app/Filament/Resources/ReturnedResource/Widgets/ReturnedChart.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Filament\Resources\ReturnedResource\Widgets;
+
+use App\Enum\StatusLoanBook;
+use App\Models\Loan;
+use Carbon\Carbon;
+use Filament\Forms\Components\DatePicker;
+use Flowframe\Trend\Trend;
+use Flowframe\Trend\TrendValue;
+use Leandrocfe\FilamentApexCharts\Widgets\ApexChartWidget;
+
+class ReturnedChart extends ApexChartWidget
+{
+    /**
+     * Chart Id
+     */
+    protected static ?string $chartId = 'returnedChart';
+
+    /**
+     * Widget Title
+     */
+    protected static ?string $heading = 'Aktivitas Pengembalian Buku';
+
+    /**
+     * Chart options (series, labels, types, size, animations...)
+     * https://apexcharts.com/docs/options
+     */
+    protected function getOptions(): array
+    {
+        $data = Trend::query(Loan::where('status', StatusLoanBook::RETURNED->value))
+            ->between(
+                start: Carbon::parse($this->filterFormData['date_start']),
+                end: Carbon::parse($this->filterFormData['date_end']),
+            )
+            ->perDay()
+            ->count();
+
+        return [
+            'chart' => [
+                'type' => 'line',
+                'height' => 300,
+            ],
+            'series' => [
+                [
+                    'name' => 'Pengembalian',
+                    'data' => $data->map(fn (TrendValue $value) => $value->aggregate),
+                ],
+            ],
+            'xaxis' => [
+                'categories' => $data->map(fn (TrendValue $value) => $value->date),
+                'labels' => [
+                    'style' => [
+                        'fontFamily' => 'inherit',
+                    ],
+                ],
+            ],
+            'yaxis' => [
+                'labels' => [
+                    'style' => [
+                        'fontFamily' => 'inherit',
+                    ],
+                ],
+            ],
+            'colors' => ['#f59e0b'],
+            'stroke' => [
+                'curve' => 'smooth',
+            ],
+        ];
+    }
+
+    protected function getFormSchema(): array
+    {
+        return [
+            DatePicker::make('date_start')
+                ->default(now()->subMonth()),
+            DatePicker::make('date_end')
+                ->default(now()),
+        ];
+    }
+}
