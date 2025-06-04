fca05c0590577a0cf548c98e0994f67d4a90c7f9
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2025-06-04 23:57:49 +0700

    FEAT: add VisitChart widget for visit activity tracking

---
+++ app/Filament/Resources/VisitResource/Widgets/VisitChart.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Filament\Resources\VisitResource\Widgets;
+
+use App\Models\VisitLog;
+use Carbon\Carbon;
+use Filament\Forms\Components\DatePicker;
+use Flowframe\Trend\Trend;
+use Flowframe\Trend\TrendValue;
+use Leandrocfe\FilamentApexCharts\Widgets\ApexChartWidget;
+
+class VisitChart extends ApexChartWidget
+{
+    /**
+     * Chart Id
+     */
+    protected static ?string $chartId = 'visitChart';
+
+    /**
+     * Widget Title
+     */
+    protected static ?string $heading = 'Aktivitas Kunjungan Perpustakaan';
+
+    /**
+     * Chart options (series, labels, types, size, animations...)
+     * https://apexcharts.com/docs/options
+     */
+    protected function getOptions(): array
+    {
+        $data = Trend::model(VisitLog::class)
+            ->between(
+                start: Carbon::parse($this->filterFormData['date_start']),
+                end: Carbon::parse($this->filterFormData['date_end']),
+            )
+            ->perMonth()
+            ->count();
+
+        return [
+            'chart' => [
+                'type' => 'line',
+                'height' => 300,
+            ],
+            'series' => [
+                [
+                    'name' => 'Kunjungan',
+                    'data' => $data->map(fn (TrendValue $value) => $value->aggregate),
+                ],
+            ],
+            'xaxis' => [
+                'categories' => $data->map(fn (TrendValue $value) => $value->date),
+                'labels' => [
+                    'style' => [
+                        'fontFamily' => 'inherit',
+                    ],
+                ],
+            ],
+            'yaxis' => [
+                'labels' => [
+                    'style' => [
+                        'fontFamily' => 'inherit',
+                    ],
+                ],
+            ],
+            'colors' => ['#f59e0b'],
+            'stroke' => [
+                'curve' => 'smooth',
+            ],
+        ];
+    }
+
+    protected function getFormSchema(): array
+    {
+        return [
+            DatePicker::make('date_start')
+                ->default(now()->subMonth()),
+            DatePicker::make('date_end')
+                ->default(now()),
+        ];
+    }
+}
