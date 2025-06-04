d8aad2b65b940794a131d571b97a106b2dd07802
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2025-06-04 23:58:19 +0700

    FEAT: add Announcement model for managing announcements

---
+++ app/Models/Announcement.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Announcement extends Model
+{
+    use HasFactory,SoftDeletes;
+
+    protected $fillable = [
+        'user_id',
+        'title',
+        'content',
+        'is_published',
+        'published_at',
+    ];
+
+    protected $casts = [
+        'is_published' => 'boolean',
+        'published_at' => 'datetime',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
