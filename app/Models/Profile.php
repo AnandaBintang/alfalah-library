6cf54f104a60b4fd9316ad0451324c9d115d87ca
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2025-06-04 23:58:52 +0700

    FEAT: add Profile model for managing user profiles

---
+++ app/Models/Profile.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Profile extends Model
+{
+    use HasFactory,SoftDeletes;
+
+    protected $fillable = [
+        'user_id',
+        'nis',
+        'nisn',
+        'class',
+        'address',
+        'phone',
+        'library_card_image_path',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
