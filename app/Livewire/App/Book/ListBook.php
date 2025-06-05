a81dfa7268179fb8d9c150fb87e85971bc9ed5af
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2025-06-05 13:03:17 +0700

    FEAT: add ListBook component for Livewire

---
+++ app/Livewire/App/Book/ListBook.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\App\Book;
+
+use Livewire\Component;
+
+class ListBook extends Component
+{
+    public function render()
+    {
+        return view('livewire.app.book.list-book');
+    }
+}
