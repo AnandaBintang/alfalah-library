c67f043eec2899329a34e114e8638bc030b0a542
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2025-06-05 13:03:27 +0700

    FEAT: add DendaDetail component for Livewire

---
+++ app/Livewire/App/User/Denda/DendaDetail.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\App\User\Denda;
+
+use Livewire\Component;
+
+class DendaDetail extends Component
+{
+    public function render()
+    {
+        return view('livewire.app.user.denda.denda-detail');
+    }
+}
