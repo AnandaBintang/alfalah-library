8f4b2a4bff6fb91d9acea665c677ba53ce6e5ad7
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2025-06-05 13:03:41 +0700

    FEAT: create DonasiDetail component for Livewire

---
+++ app/Livewire/App/User/Donasi/DonasiDetail.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\App\User\Donasi;
+
+use Livewire\Component;
+
+class DonasiDetail extends Component
+{
+    public function render()
+    {
+        return view('livewire.app.user.donasi.donasi-detail');
+    }
+}
