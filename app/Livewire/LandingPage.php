cc6cc91fdb39c7ffa853c8a8fb0bd04d8cab5764
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2025-06-05 13:08:08 +0700

    FEAT: update layout reference in LandingPage component

--- app/Livewire/LandingPage.php
+++ app/Livewire/LandingPage.php
@@ -7,7 +7,7 @@ use Livewire\Attributes\Title;
 use Livewire\Component;
 
 #[Title('Perpustakaan Al-Falah')]
-#[Layout('livewire.layouts.app')]
+#[Layout('livewire.layouts.main-app')]
 class LandingPage extends Component
 {
     public function render()
