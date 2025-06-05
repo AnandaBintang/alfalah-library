c51d9b1698c0c9bfdd240f704b6d2fc6202a1a1f
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2025-06-05 13:03:35 +0700

    FEAT: add Peminjaman component for Livewire

---
+++ app/Livewire/App/User/Peminjaman/Peminjaman.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\App\User\Peminjaman;
+
+use Livewire\Component;
+
+class Peminjaman extends Component
+{
+    public function render()
+    {
+        return view('livewire.app.user.peminjaman.peminjaman');
+    }
+}
