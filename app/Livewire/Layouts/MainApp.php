79a6630e38b65ba92b22b0bb8943f2d056ad81ab
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2025-06-05 13:03:56 +0700

    FEAT: create MainApp component for Livewire layouts

---
+++ app/Livewire/Layouts/MainApp.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Layouts;
+
+use Livewire\Component;
+
+class MainApp extends Component
+{
+    public function render()
+    {
+        return view('livewire.layouts.main-app');
+    }
+}
