470bcab93bc0d2d00637907d10c9c2f028cd7c40
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2025-06-05 13:03:13 +0700

    FEAT: create GenreBook component for Livewire

---
+++ app/Livewire/App/Book/GenreBook.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\App\Book;
+
+use Livewire\Component;
+
+class GenreBook extends Component
+{
+    public function render()
+    {
+        return view('livewire.app.book.genre-book');
+    }
+}
