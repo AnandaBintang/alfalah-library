a9a540939cf5ec3cab175832e71fc9f32386f0f6
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2025-06-06 21:12:21 +0700

    FEAT: add fines detail retrieval in Denda component

--- app/Livewire/App/User/Denda/DendaDetail.php
+++ app/Livewire/App/User/Denda/DendaDetail.php
@@ -2,12 +2,27 @@
 
 namespace App\Livewire\App\User\Denda;
 
+use App\Models\Fine;
+use Illuminate\Support\Facades\Auth;
+use Livewire\Attributes\Layout;
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
+#[Title('Detail Denda')]
+#[Layout('livewire.layouts.main-app')]
 class DendaDetail extends Component
 {
+    public $data;
+
+    public function mount($id)
+    {
+        $this->data = Fine::where('user_id', Auth::id())->where('id', $id)->first();
+    }
+
     public function render()
     {
-        return view('livewire.app.user.denda.denda-detail');
+        return view('livewire.app.user.denda.denda-detail', [
+            'fines' => $this->data,
+        ]);
     }
 }
