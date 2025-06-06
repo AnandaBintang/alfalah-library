4c69a48fb81e1e214a9b6c9646ffffa5411f7329
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2025-06-06 21:13:39 +0700

    STYLE: fix formatting in UserFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,7 +2,6 @@
 
 namespace Database\Factories;
 
-use App\Enum\RoleEnum;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
@@ -12,10 +11,10 @@ use Illuminate\Support\Str;
  */
 class UserFactory extends Factory
 {
-  /**
-   * The current password being used by the factory.
-   */
-  protected static ?string $password;
+    /**
+     * The current password being used by the factory.
+     */
+    protected static ?string $password;
 
     /**
      * Define the model's default state.
@@ -33,13 +32,13 @@ class UserFactory extends Factory
         ];
     }
 
-  /**
-   * Indicate that the model's email address should be unverified.
-   */
-  public function unverified(): static
-  {
-    return $this->state(fn(array $attributes) => [
-      'email_verified_at' => null,
-    ]);
-  }
+    /**
+     * Indicate that the model's email address should be unverified.
+     */
+    public function unverified(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'email_verified_at' => null,
+        ]);
+    }
 }
