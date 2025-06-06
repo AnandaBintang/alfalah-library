fcae56d597ea660a38dab24310664b63496593c6
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2025-06-06 21:13:42 +0700

    FEAT: enhance loans table with status enums

--- database/migrations/2025_05_08_135811_create_loans_table.php
+++ database/migrations/2025_05_08_135811_create_loans_table.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Enum\ConfirmationStatusLoanEnum;
+use App\Enum\StatusLoanBookEnum;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -18,7 +20,18 @@ return new class extends Migration
             $table->date('loan_date');
             $table->date('due_date');
             $table->date('return_date')->nullable();
-            $table->enum('status', ['borrowed', 'returned', 'overdue'])->default('borrowed');
+            $table->enum('status', [
+                StatusLoanBookEnum::BORROWED->value,
+                StatusLoanBookEnum::RETURNED->value,
+                StatusLoanBookEnum::REJECTED->value,
+                StatusLoanBookEnum::APPROVED->value,
+                StatusLoanBookEnum::OVERDUE->value,
+            ])->default(StatusLoanBookEnum::BORROWED->value);
+            $table->enum('confirmation_status', [
+                ConfirmationStatusLoanEnum::PENDING->value,
+                ConfirmationStatusLoanEnum::APPROVED->value,
+                ConfirmationStatusLoanEnum::REJECTED->value,
+            ])->default(ConfirmationStatusLoanEnum::PENDING->value);
             $table->timestamps();
             $table->softDeletes();
         });
