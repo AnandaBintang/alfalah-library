a0992f8d832d66a8d71108cc5a2a49e6dfaf5731
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2025-06-06 21:14:20 +0700

    FEAT: create migration for cart_items table

---
+++ database/migrations/2025_06_06_082928_create_cart_items_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Enum\StatusCartItemEnum;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cart_items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('cart_id')->constrained()->onDelete('cascade');
+            $table->foreignId('book_id')->constrained()->onDelete('cascade');
+            $table->integer('quantity')->default(1);
+            $table->enum('status', [
+                StatusCartItemEnum::BOOKED->value,
+                StatusCartItemEnum::APPROVED->value,
+                StatusCartItemEnum::REJECTED->value,
+            ])->default(StatusCartItemEnum::BOOKED->value);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cart_items');
+    }
+};
