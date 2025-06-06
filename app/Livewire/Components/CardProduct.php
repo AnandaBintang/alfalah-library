0cb0cf98d625068218fe6f23c1d9533b290b44db
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2025-06-06 21:12:57 +0700

    FEAT: add properties and mount method to CardProduct

--- app/Livewire/Components/CardProduct.php
+++ app/Livewire/Components/CardProduct.php
@@ -6,6 +6,22 @@ use Livewire\Component;
 
 class CardProduct extends Component
 {
+    public $cardTitle;
+
+    public $description;
+
+    public $image;
+
+    public $link;
+
+    public function mount($cardTitle = null, $description = null, $image = null, $link = null)
+    {
+        $this->image = $image;
+        $this->cardTitle = $cardTitle;
+        $this->description = $description;
+        $this->link = $link;
+    }
+
     public function render()
     {
         return view('livewire.components.card-product');
