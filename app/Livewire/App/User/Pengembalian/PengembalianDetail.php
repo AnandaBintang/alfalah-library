072a78e1f4654b83d1c59d52920fd2fb0b8e7870
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2025-06-06 21:12:42 +0700

    FEAT: implement loan return detail retrieval in PengembalianDetail

--- app/Livewire/App/User/Pengembalian/PengembalianDetail.php
+++ app/Livewire/App/User/Pengembalian/PengembalianDetail.php
@@ -2,10 +2,26 @@
 
 namespace App\Livewire\App\User\Pengembalian;
 
+use App\Enum\StatusLoanBookEnum;
+use App\Models\Loan as LoanModel;
+use Livewire\Attributes\Layout;
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
+#[Title('Detail Pengembalian')]
+#[Layout('livewire.layouts.main-app')]
 class PengembalianDetail extends Component
 {
+    public $data;
+
+    public function mount($id)
+    {
+        $this->data = LoanModel::where('user_id', Auth::id())
+            ->where('id', $id)
+            ->where('status', StatusLoanBookEnum::RETURNED->value)
+            ->first();
+    }
+
     public function render()
     {
         return view('livewire.app.user.pengembalian.pengembalian-detail');
