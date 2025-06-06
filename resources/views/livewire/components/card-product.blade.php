b6eb630070375e25b6bd87b5c2f287ff3bc1a001
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2025-06-06 21:16:10 +0700

    FEAT: implement card product component with layout

--- resources/views/livewire/components/card-product.blade.php
+++ resources/views/livewire/components/card-product.blade.php
@@ -1,3 +1,19 @@
-<div>
-    {{-- Care about people's approval and you will be their prisoner. --}}
+<div class="flex flex-col h-[400px] bg-white border border-gray-200 shadow-2xs rounded-xl overflow-hidden">
+  <img class="w-full h-48 object-cover rounded-t-xl" src="{{ asset('storage/' . $image) }}" alt="Card Image">
+  <div class="p-4 md:p-5 flex flex-col flex-1">
+    <h3 class="text-lg font-bold text-gray-800">
+      {{ \Illuminate\Support\Str::limit($cardTitle, 20) ?? "-" }}
+    </h3>
+    <p class="mt-1 text-gray-500 line-clamp-3">
+      {{ \Illuminate\Support\Str::limit($description, 100) ?? "-" }}
+    </p>
+    <div class="mt-auto pt-3">
+      <a
+        class="w-full py-2 px-3 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-hidden focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none"
+        href="{{ route('book.detail', ['id' => $link]) }}"
+      >
+        Detail
+      </a>
+    </div>
+  </div>
 </div>
