9770959d3614a307272564e88e5f4359b879d6c2
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2025-06-06 21:12:18 +0700

    FEAT: implement fines retrieval in Denda component

--- app/Livewire/App/User/Denda/Denda.php
+++ app/Livewire/App/User/Denda/Denda.php
@@ -2,12 +2,30 @@
 
 namespace App\Livewire\App\User\Denda;
 
+use Illuminate\Support\Facades\Auth;
+use Livewire\Attributes\Layout;
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
+#[Title('Denda')]
+#[Layout('livewire.layouts.main-app')]
 class Denda extends Component
 {
+    public $fines;
+
+    public function mount()
+    {
+        $user = Auth::user();
+
+        $this->fines = $user->fines()
+            ->with(['loan.book'])
+            ->get();
+    }
+
     public function render()
     {
-        return view('livewire.app.user.denda.denda');
+        return view('livewire.app.user.denda.denda', [
+            'fines' => $this->fines,
+        ]);
     }
 }
