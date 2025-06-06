df9d81a91411cbc8988f90f54dabaea18bf82838
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2025-06-06 21:15:32 +0700

    FEAT: implement logout functionality and route updates

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,20 @@
 <?php
 
+use App\Livewire\App\Book\DetailBook;
+use App\Livewire\App\Cart\Cart;
+use App\Livewire\App\User\Denda\Denda;
+use App\Livewire\App\User\Denda\DendaDetail;
+use App\Livewire\App\User\Donasi\CreateDonasi;
+use App\Livewire\App\User\Donasi\Donasi;
+use App\Livewire\App\User\Donasi\DonasiDetail;
+use App\Livewire\App\User\Peminjaman\Peminjaman;
+use App\Livewire\App\User\Peminjaman\PeminjamanDetail;
+use App\Livewire\App\User\Pengembalian\Pengembalian;
+use App\Livewire\App\User\Pengembalian\PengembalianDetail;
 use App\Livewire\Auth\Login;
-use App\Livewire\Auth\Logout;
 use App\Livewire\Auth\Register;
 use App\Livewire\LandingPage;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 // Landing page
@@ -12,4 +23,36 @@ Route::get('/', LandingPage::class)->name('index');
 // Auth
 Route::get('/login', Login::class)->name('login');
 Route::get('/register', Register::class)->name('register');
-Route::post('/logout', Logout::class)->name('logout');
+Route::post('/logout', function () {
+    Auth::logout();
+    session()->invalidate();
+    session()->regenerateToken();
+
+    return redirect()->route('login');
+})->name('logout');
+
+Route::middleware('auth')->group(function () {
+    // Book
+    Route::get('/book', \App\Livewire\App\Book\Book::class)->name('book.index');
+    Route::get('/book/{id}', DetailBook::class)->name('book.detail');
+
+    // Cart
+    Route::get('/cart', Cart::class)->name('cart.index');
+
+    // Riwayat pengembalian
+    Route::get('/pengembalian', Pengembalian::class)->name('pengembalian.index');
+    Route::get('/pengembalian/{id}', PengembalianDetail::class)->name('pengembalian.detail');
+
+    // Riwayat peminjaman
+    Route::get('peminjaman', Peminjaman::class)->name('peminjaman.index');
+    Route::get('peminjaman/{id}', PeminjamanDetail::class)->name('peminjaman.detail');
+
+    // Riwayat donasi
+    Route::get('/donasi', Donasi::class)->name('donasi.index');
+    Route::get('/donasi/{id}', DonasiDetail::class)->name('donasi.detail');
+    Route::post('/donasi/new', CreateDonasi::class)->name('donasi.new');
+
+    // Riwayat denda
+    Route::get('/denda', Denda::class)->name('denda.index');
+    Route::get('/denda/{id}', DendaDetail::class)->name('denda.detail');
+});
