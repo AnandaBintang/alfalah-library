583ac4eebeb8ca39cb2afba684ab9d2250e1ead3
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2025-06-06 21:12:36 +0700

    FEAT: add loan detail retrieval in PeminjamanDetail

--- app/Livewire/App/User/Peminjaman/PeminjamanDetail.php
+++ app/Livewire/App/User/Peminjaman/PeminjamanDetail.php
@@ -2,10 +2,27 @@
 
 namespace App\Livewire\App\User\Peminjaman;
 
+use App\Enum\StatusLoanBookEnum;
+use App\Models\Loan as LoanModel;
+use Illuminate\Support\Facades\Auth;
+use Livewire\Attributes\Layout;
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
+#[Title('Detail Peminjaman')]
+#[Layout('livewire.layouts.main-app')]
 class PeminjamanDetail extends Component
 {
+    public $data;
+
+    public function mount($id)
+    {
+        $this->data = LoanModel::where('user_id', Auth::id())
+            ->where('id', $id)
+            ->where('status', StatusLoanBookEnum::BORROWED->value)
+            ->first();
+    }
+
     public function render()
     {
         return view('livewire.app.user.peminjaman.peminjaman-detail');
