3bf9844024b003d123d9803ff2802d64f702c6b6
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2025-06-06 21:16:12 +0700

    FEAT: add announcement banner component

---
+++ resources/views/livewire/components/announcement-banner.blade.php
@@ -0,0 +1,5 @@
+<div class="bg-yellow-100 text-yellow-800 py-3 px-4 text-sm font-medium overflow-hidden relative" wire:poll>
+  <div class="animate-marquee whitespace-nowrap">
+    {{ $announcement->content ?? "📢 Announcement" }}
+  </div>
+</div>
