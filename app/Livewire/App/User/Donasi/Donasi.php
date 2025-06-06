3b2896c91529b50beda8c780cc6a8f0ecc26a1a1
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2025-06-06 21:12:27 +0700

    FEAT: implement data retrieval in Donasi component

--- app/Livewire/App/User/Donasi/Donasi.php
+++ app/Livewire/App/User/Donasi/Donasi.php
@@ -2,12 +2,27 @@
 
 namespace App\Livewire\App\User\Donasi;
 
+use Illuminate\Support\Facades\Auth;
+use Livewire\Attributes\Layout;
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
+#[Title('Donasi')]
+#[Layout('livewire.layouts.main-app')]
 class Donasi extends Component
 {
+    public $data;
+
+    public function mount()
+    {
+        $user = Auth::user();
+        $this->data = $user->donations()->get();
+    }
+
     public function render()
     {
-        return view('livewire.app.user.donasi.donasi');
+        return view('livewire.app.user.donasi.donasi', [
+            'datas' => $this->data,
+        ]);
     }
 }
