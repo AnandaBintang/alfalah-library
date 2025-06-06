e280381f5d228bc6a25ac9a34ea07cd599b8aba2
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2025-06-06 21:14:03 +0700

    REFACTOR: update donation status to use ApprovalStatusEnum

--- database/migrations/2025_05_08_135812_create_donations_table.php
+++ database/migrations/2025_05_08_135812_create_donations_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Enum\ApprovalStatusEnum;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -19,7 +20,11 @@ return new class extends Migration
             $table->text('description')->nullable();
             $table->integer('quantity')->default(1);
             $table->date('donation_date');
-            $table->enum('status', ['pending', 'approved', 'rejected'])->default('pending');
+            $table->enum('status', [
+                ApprovalStatusEnum::PENDING->value,
+                ApprovalStatusEnum::APPROVED->value,
+                ApprovalStatusEnum::REJECTED->value,
+            ])->default(ApprovalStatusEnum::PENDING->value);
             $table->timestamps();
             $table->softDeletes();
         });
