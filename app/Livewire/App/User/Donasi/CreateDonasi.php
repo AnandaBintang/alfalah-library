2a7193045b6d214faf73f435d33c11f8319cc17a
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2025-06-06 21:12:24 +0700

    FEAT: add title and layout attributes to CreateDonasi

--- app/Livewire/App/User/Donasi/CreateDonasi.php
+++ app/Livewire/App/User/Donasi/CreateDonasi.php
@@ -2,8 +2,12 @@
 
 namespace App\Livewire\App\User\Donasi;
 
+use Livewire\Attributes\Layout;
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
+#[Title('New Donasi')]
+#[Layout('livewire.layouts.main-app')]
 class CreateDonasi extends Component
 {
     public function render()
