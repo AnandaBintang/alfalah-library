ecddb64fba955a3149373e6c7033f109481fccea
Gabriel Azarya <gabrielkrishna5@gmail.com>
2025-06-07 21:13:11 +0700

    FIX: fix landingPage layouts

--- resources/views/livewire/landing-page.blade.php
+++ resources/views/livewire/landing-page.blade.php
@@ -5,25 +5,3 @@
         @include('livewire.components.landingpage.location')
         @include('livewire.components.landingpage.footer')
 </div>
-
-<script>
-document.addEventListener('DOMContentLoaded', function() {
-    const observer = new IntersectionObserver(entries => {
-        entries.forEach(entry => {
-            const id = entry.target.getAttribute('id');
-            const link = document.querySelector(`a[href="#${id}"]`);
-            if (link) {
-                if (entry.isIntersecting) {
-                    link.classList.add('active');
-                } else {
-                    link.classList.remove('active');
-                }
-            }
-        });
-    }, { threshold: 0.6 });
-
-    document.querySelectorAll('div[id]').forEach(section => {
-        observer.observe(section);
-    });
-});
-</script>
