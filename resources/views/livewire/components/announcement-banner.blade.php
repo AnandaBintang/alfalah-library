84402ad69a9527b3ca0ed3842f36fdcc622676d2
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2025-06-08 12:15:52 +0700

    FIX: update wire:poll to wire:poll.visible

--- resources/views/livewire/components/announcement-banner.blade.php
+++ resources/views/livewire/components/announcement-banner.blade.php
@@ -1,4 +1,4 @@
-<div class="bg-yellow-100 text-yellow-800 py-3 px-4 text-sm font-medium overflow-hidden relative" wire:poll>
+<div class="bg-yellow-100 text-yellow-800 py-3 px-4 text-sm font-medium overflow-hidden relative" wire:poll.visible>
   <div class="animate-marquee whitespace-nowrap">
     {{ $announcement->content ?? "📢 Announcement" }}
   </div>
