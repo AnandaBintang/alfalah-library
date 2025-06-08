b68a3e588acdb26043f14867b42d2ae5c7d960f0
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2025-06-08 14:05:08 +0700

    FEAT: add profile route and reorganize route structure

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,7 @@ use App\Livewire\Auth\Register;
 use App\Livewire\LandingPage;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Livewire\App\User\Profile\Profile;
 
 // Landing page
 Route::get('/', LandingPage::class)->name('index');
@@ -26,39 +27,43 @@ Route::get('/', LandingPage::class)->name('index');
 Route::get('/login', Login::class)->name('login');
 Route::get('/register', Register::class)->name('register');
 Route::post('/logout', function () {
-    Auth::logout();
-    session()->invalidate();
-    session()->regenerateToken();
+  Auth::logout();
+  session()->invalidate();
+  session()->regenerateToken();
 
-    return redirect()->route('login');
+  return redirect()->route('login');
 })->name('logout');
 
 Route::middleware('auth')->group(function () {
-    // Book
-    Route::get('/book', \App\Livewire\App\Book\Book::class)->name('book.index');
-    Route::get('/book/{id}', DetailBook::class)->name('book.detail');
 
-    // Cart
-    Route::get('/cart', Cart::class)->name('cart.index');
+  // Profile
+  Route::get('/profile', Profile::class)->name('profile.index');;
 
-    // Riwayat pengembalian
-    Route::get('/pengembalian', Pengembalian::class)->name('pengembalian.index');
-    Route::get('/pengembalian/{id}', PengembalianDetail::class)->name('pengembalian.detail');
+  // Book
+  Route::get('/book', \App\Livewire\App\Book\Book::class)->name('book.index');
+  Route::get('/book/{id}', DetailBook::class)->name('book.detail');
 
-    // Riwayat peminjaman
-    Route::get('/peminjaman', Peminjaman::class)->name('peminjaman.index');
-    Route::get('/peminjaman/{id}', PeminjamanDetail::class)->name('peminjaman.detail');
+  // Cart
+  Route::get('/cart', Cart::class)->name('cart.index');
 
-    // Riwayat donasi
-    Route::get('/donasi/store', CreateDonasi::class)->name('donasi.store');
-    Route::get('/donasi', Donasi::class)->name('donasi.index');
-    Route::get('/donasi/{id}', DonasiDetail::class)->name('donasi.detail');
+  // Riwayat pengembalian
+  Route::get('/pengembalian', Pengembalian::class)->name('pengembalian.index');
+  Route::get('/pengembalian/{id}', PengembalianDetail::class)->name('pengembalian.detail');
 
-    // Riwayat denda
-    Route::get('/denda', Denda::class)->name('denda.index');
-    Route::get('/denda/{id}', DendaDetail::class)->name('denda.detail');
+  // Riwayat peminjaman
+  Route::get('/peminjaman', Peminjaman::class)->name('peminjaman.index');
+  Route::get('/peminjaman/{id}', PeminjamanDetail::class)->name('peminjaman.detail');
 
-    // Riwayat extenion pinjaman
-    Route::get('/perpanjang-peminjaman', PerpanjangPeminjaman::class)->name('perpanjang-peminjaman.index');
-    Route::get('/perpanjang-peminjaman/{id}', PerpanjangPeminjamanDetail::class)->name('perpanjang-peminjaman.detail');
+  // Riwayat donasi
+  Route::get('/donasi/store', CreateDonasi::class)->name('donasi.store');
+  Route::get('/donasi', Donasi::class)->name('donasi.index');
+  Route::get('/donasi/{id}', DonasiDetail::class)->name('donasi.detail');
+
+  // Riwayat denda
+  Route::get('/denda', Denda::class)->name('denda.index');
+  Route::get('/denda/{id}', DendaDetail::class)->name('denda.detail');
+
+  // Riwayat extenion pinjaman
+  Route::get('/perpanjang-peminjaman', PerpanjangPeminjaman::class)->name('perpanjang-peminjaman.index');
+  Route::get('/perpanjang-peminjaman/{id}', PerpanjangPeminjamanDetail::class)->name('perpanjang-peminjaman.detail');
 });
