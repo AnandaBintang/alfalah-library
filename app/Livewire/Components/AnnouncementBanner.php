d75492f08215204bb0d3284cb272ed3283964027
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2025-06-08 12:14:48 +0700

    REFACTOR: improve announcement query logic

--- app/Livewire/Components/AnnouncementBanner.php
+++ app/Livewire/Components/AnnouncementBanner.php
@@ -3,6 +3,7 @@
 namespace App\Livewire\Components;
 
 use App\Models\Announcement;
+use Carbon\Carbon;
 use Livewire\Component;
 
 class AnnouncementBanner extends Component
@@ -10,7 +11,9 @@ class AnnouncementBanner extends Component
     public function render()
     {
         return view('livewire.components.announcement-banner', [
-            'announcement' => Announcement::where('is_published', 1)->first(),
+            'announcement' => Announcement::where('is_published', 1)
+                ->whereDate('published_at', Carbon::today())
+                ->first(),
         ]);
     }
 }
