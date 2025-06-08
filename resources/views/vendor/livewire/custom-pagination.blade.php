05bfe724f4a37aa0566c8c231498da976be3768d
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2025-06-08 12:15:50 +0700

    FIX: correct wireclick attribute in pagination button

--- resources/views/vendor/livewire/custom-pagination.blade.php
+++ resources/views/vendor/livewire/custom-pagination.blade.php
@@ -11,7 +11,7 @@
       <span class="hidden sm:block">Previous</span>
     </button>
   @else
-    <button wire:click="previousPage" type="button"
+    <button wireclick="previousPage" type="button"
             class="min-h-9.5 min-w-9.5 py-2 px-2.5 inline-flex justify-center items-center gap-x-1.5 text-sm first:rounded-s-lg last:rounded-e-lg border border-gray-200 text-gray-800 hover:bg-gray-100 focus:outline-hidden focus:bg-gray-100"
             aria-label="Previous">
       <svg class="shrink-0 size-3.5" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
