7255a93644bf8fd99f907da9dc6af1bd1b91a346
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2025-06-08 14:03:58 +0700

    FEAT: add interval selection to LoanChart widget

--- app/Filament/Resources/LoanChartResource/Widgets/LoanChart.php
+++ app/Filament/Resources/LoanChartResource/Widgets/LoanChart.php
@@ -6,76 +6,94 @@ use App\Enum\StatusLoanBookEnum;
 use App\Models\Loan;
 use Carbon\Carbon;
 use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\Select;
 use Flowframe\Trend\Trend;
 use Flowframe\Trend\TrendValue;
+use Illuminate\Support\Str;
 use Leandrocfe\FilamentApexCharts\Widgets\ApexChartWidget;
 
 class LoanChart extends ApexChartWidget
 {
-    /**
-     * Chart Id
-     */
-    protected static ?string $chartId = 'loanChart';
+  /**
+   * Chart Id
+   */
+  protected static ?string $chartId = 'loanChart';
 
-    /**
-     * Widget Title
-     */
-    protected static ?string $heading = 'Aktivitas Peminjaman Buku';
+  /**
+   * Widget Title
+   */
+  protected static ?string $heading = 'Aktivitas Peminjaman Buku';
 
-    /**
-     * Chart options (series, labels, types, size, animations...)
-     * https://apexcharts.com/docs/options
-     */
-    protected function getOptions(): array
-    {
-        $data = Trend::query(Loan::where('status', StatusLoanBookEnum::BORROWED->value))
-            ->between(
-                start: Carbon::parse($this->filterFormData['date_start']),
-                end: Carbon::parse($this->filterFormData['date_end']),
-            )
-            ->perMonth()
-            ->count();
+  /**
+   * Chart options (series, labels, types, size, animations...)
+   * https://apexcharts.com/docs/options
+   */
+  protected function getOptions(): array
+  {
+    $interval = $this->filterFormData['interval'] ?? 'month';
+    $start = $this->filterFormData['date_start'] ?? now()->subMonth();
+    $end = $this->filterFormData['date_end'] ?? now();
 
-        return [
-            'chart' => [
-                'type' => 'line',
-                'height' => 300,
-            ],
-            'series' => [
-                [
-                    'name' => 'Peminjaman',
-                    'data' => $data->map(fn (TrendValue $value) => $value->aggregate),
-                ],
-            ],
-            'xaxis' => [
-                'categories' => $data->map(fn (TrendValue $value) => $value->date),
-                'labels' => [
-                    'style' => [
-                        'fontFamily' => 'inherit',
-                    ],
-                ],
-            ],
-            'yaxis' => [
-                'labels' => [
-                    'style' => [
-                        'fontFamily' => 'inherit',
-                    ],
-                ],
-            ],
-            'colors' => ['#f59e0b'],
-            'stroke' => [
-                'curve' => 'smooth',
-            ],
-        ];
-    }
+    $method = Str::camel('per_' . $interval);
 
-    protected function getFormSchema(): array
-    {
-        return [
-            DatePicker::make('date_start')
-                ->default(now()->subMonth()),
-            DatePicker::make('date_end')
-                ->default(now()),
-        ];
-    }
+    $data = Trend::query(
+      Loan::where('loan_status', StatusLoanBookEnum::BORROWED->value)
+    )
+      ->between(
+        start: Carbon::parse($start)->startOfDay(),
+        end: Carbon::parse($end)->endOfDay(),
+      )
+      ->{$method}()
+      ->count();
+
+    return [
+      'chart' => [
+        'type' => 'line',
+        'height' => 300,
+      ],
+      'series' => [
+        [
+          'name' => 'Peminjaman',
+          'data' => $data->map(fn(TrendValue $value) => $value->aggregate),
+        ],
+      ],
+      'xaxis' => [
+        'categories' => $data->map(fn(TrendValue $value) => $value->date),
+        'labels' => [
+          'style' => [
+            'fontFamily' => 'inherit',
+          ],
+        ],
+      ],
+      'yaxis' => [
+        'labels' => [
+          'style' => [
+            'fontFamily' => 'inherit',
+          ],
+        ],
+      ],
+      'colors' => ['#f59e0b'],
+      'stroke' => [
+        'curve' => 'smooth',
+      ],
+    ];
+  }
+
+  protected function getFormSchema(): array
+  {
+    return [
+      Select::make('interval')
+        ->label('Tampilan Data')
+        ->default('month')
+        ->options([
+          'day' => 'Harian',
+          'week' => 'Mingguan',
+          'month' => 'Bulanan',
+        ]),
+      DatePicker::make('date_start')
+        ->default(now()->subMonth()),
+      DatePicker::make('date_end')
+        ->default(now()),
+    ];
+  }
 }
