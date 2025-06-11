1f7a1da429d10d646d855db3f7664fdb8ae46ee2
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2025-06-11 22:16:11 +0700

    FEAT: replace notification system with LivewireAlert

--- app/Livewire/App/Cart/CartItem.php
+++ app/Livewire/App/Cart/CartItem.php
@@ -3,99 +3,105 @@
 namespace App\Livewire\App\Cart;
 
 use App\Enum\StatusCartEnum;
+use App\Enum\StatusCartItemEnum;
 use Illuminate\Support\Facades\DB;
+use Jantinnerezo\LivewireAlert\Facades\LivewireAlert;
 use Livewire\Attributes\Layout;
 use Livewire\Component;
-use WireUi\Traits\WireUiActions;
 
 #[Layout('livewire.layouts.main-app')]
 class CartItem extends Component
 {
-    use WireUiActions;
 
-    public $item;
+  public $item;
 
-    public function mount($cartItem)
-    {
-        $this->item = $cartItem->load('book');
-    }
+  public function mount($cartItem)
+  {
+    $this->item = $cartItem->load('book');
+  }
 
-    public function increment()
-    {
-        DB::transaction(function () {
-            $book = $this->item->book()->lockForUpdate()->first();
+  public function increment()
+  {
+    DB::transaction(function () {
+      $book = $this->item->book()->lockForUpdate()->first();
 
-            if ($book->stock > 0) {
-                $this->item->quantity++;
-                $book->stock--;
+      if ($book->stock > 0) {
+        $this->item->quantity++;
+        $book->stock--;
 
-                $book->save();
-                $this->item->save();
+        $book->save();
+        $this->item->save();
 
-                $this->dispatch('refreshCart');
-            } else {
-                $this->notification()->send([
-                    'title' => 'Stok Habis',
-                    'description' => 'Buku tidak tersedia.',
-                    'icon' => 'error',
-                ]);
-            }
-        });
-    }
+        $this->dispatch('refreshCart');
+      } else {
+        LivewireAlert::title('Stock Habis!')
+          ->text('Buku tidak tersedia.')
+          ->position('center')
+          ->timer(5500)
+          ->error()
+          ->show();
+      }
+    });
+  }
 
-    public function decrement()
-    {
-        DB::transaction(function () {
-            $book = $this->item->book()->lockForUpdate()->first();
+  public function decrement()
+  {
+    DB::transaction(function () {
+      $book = $this->item->book()->lockForUpdate()->first();
 
-            if ($this->item->quantity > 1) {
-                $this->item->quantity--;
-                $book->stock++;
+      if ($this->item->quantity > 1) {
+        $this->item->quantity--;
+        $book->stock++;
 
-                $this->item->book->save();
-                $this->item->save();
-                $this->dispatch('refreshCart');
-            }
-        });
+        $this->item->book->save();
+        $this->item->save();
+        $this->dispatch('refreshCart');
+      }
+    });
 
-    }
+  }
 
-    public function remove()
-    {
-        DB::transaction(function () {
-            $book = $this->item->book()->lockForUpdate()->first();
+  public function remove()
+  {
+    DB::transaction(function () {
+      $book = $this->item->book()->lockForUpdate()->first();
 
-            $this->item->update([
-                'status' => StatusCartEnum::CANCELLED->value,
-            ]);
+      $this->item->update([
+        'status' => StatusCartItemEnum::REJECTED->value,
+      ]);
 
-            $book->stock += $this->item->quantity;
-            $book->save();
+      $book->stock += $this->item->quantity;
+      $book->save();
 
-            $this->item->delete();
+      $this->item->delete();
 
-            $this->dispatch('refreshCart');
-            $this->notification()->send([
-                'icon' => 'success',
-                'title' => 'Success',
-                'description' => 'Berhasil menghapus item.',
-            ]);
-        });
-    }
+      $this->dispatch('refreshCart');
 
-    public function confirmRemove(): void
-    {
-        $this->notification()->confirm([
-            'title' => 'Delete Item?',
-            'description' => 'Apakah anda yakin ingin menghapus item ini?',
-            'acceptLabel' => 'Yes',
-            'method' => 'remove',
-            'params' => 'Saved',
-        ]);
-    }
 
-    public function render()
-    {
-        return view('livewire.app.cart.cart-item');
-    }
+      LivewireAlert::title('Success!')
+        ->text('Berhasil menghapus item.')
+        ->position('center')
+        ->timer(5500)
+        ->success()
+        ->show();
+    });
+  }
+
+  public function confirmRemove(): void
+  {
+    LivewireAlert::title('Hapus buku?')
+      ->text('Anda yakin ingin menghapus buku ini?')
+      ->position('center')
+      ->timer(5500)
+      ->success()
+      ->onConfirm('remove')
+      ->withConfirmButton('Yes')
+      ->withCancelButton('No')
+      ->show();
+  }
+
+  public function render()
+  {
+    return view('livewire.app.cart.cart-item');
+  }
 }
