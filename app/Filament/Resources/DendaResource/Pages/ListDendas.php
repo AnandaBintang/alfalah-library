0c6b91490b9c7315e350dea230bba73485dab8de
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2025-07-02 20:38:43 +0700

    STYLE: improve code formatting in ListDendas.php

--- app/Filament/Resources/DendaResource/Pages/ListDendas.php
+++ app/Filament/Resources/DendaResource/Pages/ListDendas.php
@@ -8,12 +8,14 @@ use Filament\Resources\Pages\ListRecords;
 
 class ListDendas extends ListRecords
 {
-    protected static string $resource = DendaResource::class;
+  protected static string $resource = DendaResource::class;
+  protected static ?string $title = 'Daftar Denda';
 
-    protected function getHeaderActions(): array
-    {
-        return [
-            Actions\CreateAction::make(),
-        ];
-    }
+
+  protected function getHeaderActions(): array
+  {
+    return [
+      Actions\CreateAction::make(),
+    ];
+  }
 }
