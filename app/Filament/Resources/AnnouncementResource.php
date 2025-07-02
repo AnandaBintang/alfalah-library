02c3f697400aa6dd1e25b0fb8880a70e87c24b61
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2025-07-02 20:38:09 +0700

    CHORE: add missing Auth facade import in AnnouncementResource

--- app/Filament/Resources/AnnouncementResource.php
+++ app/Filament/Resources/AnnouncementResource.php
@@ -10,6 +10,7 @@ use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
+use Illuminate\Support\Facades\Auth;
 
 class AnnouncementResource extends Resource
 {
