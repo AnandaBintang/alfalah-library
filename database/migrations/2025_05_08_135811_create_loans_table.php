62420a1582f18a3ffb3082c080cc37136858ed04
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2025-07-11 20:35:25 +0700

    REFACTOR: Improve formatting in loans migration file

--- database/migrations/2025_05_08_135811_create_loans_table.php
+++ database/migrations/2025_05_08_135811_create_loans_table.php
@@ -7,45 +7,46 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('loans', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
-            $table->foreignId('book_id')->constrained()->onDelete('cascade');
-            $table->date('loan_date');
-            $table->date('due_date');
-            $table->date('return_date')->nullable();
-            $table->enum('timeline_status', [
-                TimelineStatusEnum::OVERDUE->value,
-                TimelineStatusEnum::ONTIME->value,
-                TimelineStatusEnum::PENDING->value,
-            ])->default(TimelineStatusEnum::PENDING->value);
-            $table->enum('loan_status', [
-                StatusLoanBookEnum::BORROWED->value,
-                StatusLoanBookEnum::RETURNED->value,
-                StatusLoanBookEnum::PENDING->value,
-            ])->default(StatusLoanBookEnum::PENDING->value);
-            $table->enum('confirmation_status', [
-                ConfirmationStatusLoanEnum::PENDING->value,
-                ConfirmationStatusLoanEnum::REJECTED->value,
-                ConfirmationStatusLoanEnum::APPROVED->value,
-            ])->default(ConfirmationStatusLoanEnum::PENDING->value);
-            $table->timestamps();
-            $table->softDeletes();
-        });
-    }
+return new class extends Migration {
+  /**
+   * Run the migrations.
+   */
+  public function up(): void
+  {
+    Schema::create('loans', function (Blueprint $table) {
+      $table->id();
+      $table->foreignId('user_id')->constrained()->onDelete('cascade');
+      $table->foreignId('book_id')->constrained()->onDelete('cascade');
+      $table->date('loan_date');
+      $table->date('due_date');
+      $table->date('return_date')->nullable();
+      $table->enum('timeline_status', [
+        TimelineStatusEnum::OVERDUE->value,
+        TimelineStatusEnum::ONTIME->value,
+        TimelineStatusEnum::PENDING->value,
+        TimelineStatusEnum::REJECTED->value,
+      ])->default(TimelineStatusEnum::PENDING->value);
+      $table->enum('loan_status', [
+        StatusLoanBookEnum::BORROWED->value,
+        StatusLoanBookEnum::RETURNED->value,
+        StatusLoanBookEnum::PENDING->value,
+        StatusLoanBookEnum::REJECTED->value
+      ])->default(StatusLoanBookEnum::PENDING->value);
+      $table->enum('confirmation_status', [
+        ConfirmationStatusLoanEnum::PENDING->value,
+        ConfirmationStatusLoanEnum::REJECTED->value,
+        ConfirmationStatusLoanEnum::APPROVED->value,
+      ])->default(ConfirmationStatusLoanEnum::PENDING->value);
+      $table->timestamps();
+      $table->softDeletes();
+    });
+  }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('loans');
-    }
+  /**
+   * Reverse the migrations.
+   */
+  public function down(): void
+  {
+    Schema::dropIfExists('loans');
+  }
 };
