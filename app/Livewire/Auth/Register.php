a53bd5aec665f44949853be8c8dbc8b48b7e0d4a
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2025-07-11 20:35:06 +0700

    STYLE: Update title attribute for Register component

--- app/Livewire/Auth/Register.php
+++ app/Livewire/Auth/Register.php
@@ -11,7 +11,7 @@ use Livewire\Attributes\Title;
 use Livewire\Component;
 
 #[Layout('livewire.layouts.auth-app')]
-#[Title('Buku')]
+#[Title('Register')]
 class Register extends Component
 {
     public $name = '';
