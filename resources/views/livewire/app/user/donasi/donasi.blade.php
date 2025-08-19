f9a976298974495b729f4ebec7ce614b5e58ccaa
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2025-08-19 16:06:20 +0700

    FEAT: Add status indicators for donation items

--- resources/views/livewire/app/user/donasi/donasi.blade.php
+++ resources/views/livewire/app/user/donasi/donasi.blade.php
@@ -9,30 +9,59 @@
             <table class="min-w-full divide-y divide-gray-200">
               <thead>
               <tr>
-                <th scope="col" class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">Nama Donatur</th>
+                <th scope="col" class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">Nama Donatur
+                </th>
                 <th scope="col" class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">Cover</th>
                 <th scope="col" class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">Judul Buku</th>
                 <th scope="col" class="px-6 py-3 text-end text-xs font-medium text-gray-500 uppercase">Jumlah</th>
-                <th scope="col" class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">Status Donasi</th>
-                <th scope="col" class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">Tanggal Pengajuan</th>
-                <th scope="col" class="px-6 py-3 text-end text-xs font-medium text-gray-500 uppercase">Detail Donasi</th>
+                <th scope="col" class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">Status Donasi
+                </th>
+                <th scope="col" class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">Tanggal
+                  Pengajuan
+                </th>
+                <th scope="col" class="px-6 py-3 text-end text-xs font-medium text-gray-500 uppercase">Detail Donasi
+                </th>
               </tr>
               </thead>
               <tbody class="divide-y divide-gray-200">
               @forelse($datas as $data)
                 <tr>
-                  <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800">{{ \Illuminate\Support\Facades\Auth::user()->name }}</td>
+                  <td
+                    class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800">{{ \Illuminate\Support\Facades\Auth::user()->name }}</td>
                   <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-800">
                     <img src="{{ asset('storage/' . $data->image) }}" alt="image" class="w-16 h-auto rounded">
                   </td>
                   <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-800">{{ $data->item_name }}</td>
                   <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-800 text-end">{{ $data->quantity }}</td>
-                  <td class="px-6 py-4 whitespace-nowrap text-sm font-semibold text-gray-800">
-                    {{ ucfirst($data->status) }}
-                  </td>
-                  <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-800">{{ \Carbon\Carbon::parse($data->donation_date)->format('d M Y') }}</td>
+                  @if($data->status == \App\Enum\ApprovalStatusEnum::PENDING->value )
+                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-800 text-center">
+                      <span
+                        class="inline-flex items-center justify-center py-1.5 px-3 rounded-full text-xs font-medium bg-yellow-500 text-white">
+                        {{ ucfirst($data->status) }}
+                      </span>
+                    </td>
+                  @elseif($data->status == \App\Enum\ApprovalStatusEnum::APPROVED->value)
+                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-800 text-center">
+                      <span
+                        class="inline-flex items-center justify-center py-1.5 px-3 rounded-full text-xs font-medium bg-teal-500 text-white">
+                        {{ ucfirst($data->status) }}
+                      </span>
+                    </td>
+                  @else
+                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-800 text-center">
+                      <span
+                        class="inline-flex items-center justify-center py-1.5 px-3 rounded-full text-xs font-medium bg-red-500 text-white">
+                        {{ ucfirst($data->status) }}
+                      </span>
+                    </td>
+                  @endif
+
+
+                  <td
+                    class="px-6 py-4 whitespace-nowrap text-sm text-gray-800">{{ \Carbon\Carbon::parse($data->donation_date)->format('d M Y') }}</td>
                   <td class="px-6 py-4 whitespace-nowrap text-end text-sm font-medium">
-                    <a href="{{ route('donasi.detail', ['id' => $data->id]) }}" class="text-blue-600 hover:text-blue-800">Detail</a>
+                    <a href="{{ route('donasi.detail', ['id' => $data->id]) }}"
+                       class="text-blue-600 hover:text-blue-800">Detail</a>
                   </td>
                 </tr>
               @empty
