14f315b1456f5879aee8a279a31e334efd90bcfa
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2025-08-20 16:36:02 +0700

    FEAT: Prepare for user import functionality integration

--- app/Filament/Resources/UserResource/Pages/ListUsers.php
+++ app/Filament/Resources/UserResource/Pages/ListUsers.php
@@ -3,14 +3,20 @@
 namespace App\Filament\Resources\UserResource\Pages;
 
 use App\Enum\RoleEnum;
+use App\Filament\Imports\UserImporter;
 use App\Filament\Resources\UserResource;
 use Filament\Actions;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Tables\Actions\ImportAction;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
 
 class ListUsers extends ListRecords
 {
-    protected static string $resource = UserResource::class;
+  protected static string $resource = UserResource::class;
 
   public function mount(): void
   {
@@ -21,9 +27,33 @@ class ListUsers extends ListRecords
 
 
   protected function getHeaderActions(): array
-    {
-        return [
-            Actions\CreateAction::make(),
-        ];
-    }
+  {
+    return [
+//      \EightyNine\ExcelImport\ExcelImportAction::make()
+//        ->sampleExcel(
+//          sampleData: [
+//            'name' => 'Budi Gunadi',
+//            'email' => 'budigunadi@gmail.com',
+//            'password' => 'password',
+//          ],
+//          fileName: 'sample-users.xlsx',
+//          sampleButtonLabel: 'Download Sample',
+//        )
+//        ->afterImport(function ($records) {
+//          foreach ($records as $record) {
+//            $record->assignRole(\App\Enum\RoleEnum::SISWA->value);
+//          }
+//        })
+//        ->validateUsing([
+//          'name' => 'required|string',
+//          'email' => 'required|email|unique:users,email',
+//          'password' => 'required|string',
+//        ])
+//        ->color("primary")
+//        ->label("Import Excel"),
+      Actions\CreateAction::make(),
+
+
+    ];
+  }
 }
