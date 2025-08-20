5f439f06f36cfe7227df6a596592505dbcb288e4
AnandaBintang <anandabintang4@gmail.com>
2025-08-20 23:28:16 +0700

    fix: changing login logic using is_active

--- app/Livewire/Auth/Login.php
+++ app/Livewire/Auth/Login.php
@@ -44,23 +44,24 @@ class Login extends Component
       return;
     }
 
-    // Check apakah user belum pernah login
-    if ($user->expires_at == null && $user->activated_at == null) {
-      $user->is_active = 1;
-      $user->activated_at = now();
-      $user->expires_at = now()->addYear(3);
-
-      $user->save();
-    }
-
-    if ($user->expires_at < now() && $user->expires_at != null && $user->activated_at != null ) {
-      LivewireAlert::title('Oops!')
-        ->text('Sepertinya akun kamu sudah nonaktif, segera hubungi administrator.')
+    if ($user->is_active == 0) {
+      LivewireAlert::title('Akun Tidak Aktif!')
+        ->text('Akun Anda tidak aktif. Silakan hubungi administrator untuk mengaktifkan akun.')
         ->withConfirmButton('Ok')
         ->error()
         ->timer(8000)
         ->show();
       return;
+    }
+
+    if ($user->activated_at == null) {
+      $user->activated_at = now();
+      $user->save();
+    }
+
+    if ($user->expires_at == null) {
+      $user->expires_at = now()->addYear(3);
+      $user->save();
     }
 
     if (Auth::attempt(['email' => $this->email, 'password' => $this->password], $this->remember)) {
@@ -69,7 +70,7 @@ class Login extends Component
       return redirect()->route('book.index');
     } else {
       LivewireAlert::title('Error!')
-        ->text('Account atau password salah.')
+        ->text('Email atau password salah.')
         ->position('top-end')
         ->timer(5500)
         ->error()
--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -9,6 +9,7 @@ use App\Filament\Resources\ReturnedResource\Widgets\ReturnedChart;
 use App\Filament\Resources\VisitResource\Widgets\VisitChart;
 use App\Filament\Widgets\TopVisitors;
 use App\Filament\Widgets\UserStats;
+use App\Http\Middleware\CheckActiveUser;
 use App\Http\Middleware\EnsureUserHasAdminRole;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
@@ -77,6 +78,7 @@ class AdminPanelProvider extends PanelProvider
       ])
       ->authMiddleware([
         Authenticate::class,
+        CheckActiveUser::class,
         EnsureUserHasAdminRole::class,
       ]);
   }
