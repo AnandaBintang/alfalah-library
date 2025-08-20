1616e716d418dcbf45517bd6c977ae4965603638
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2025-08-20 10:16:02 +0700

    REFACTOR: Improve code formatting and consistency

--- app/Filament/Resources/AnnouncementResource.php
+++ app/Filament/Resources/AnnouncementResource.php
@@ -14,78 +14,82 @@ use Illuminate\Support\Facades\Auth;
 
 class AnnouncementResource extends Resource
 {
-    protected static ?string $model = Announcement::class;
+  protected static ?string $model = Announcement::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-megaphone';
+  protected static ?string $navigationIcon = 'heroicon-o-megaphone';
 
-    public static function form(Form $form): Form
-    {
-        return $form
-            ->schema([
-                Forms\Components\TextInput::make('title')
-                    ->required()
-                    ->maxLength(255)
-                    ->label('Title'),
-                Forms\Components\TextInput::make('content')
-                    ->required()
-                    ->maxLength(255),
-                Forms\Components\Radio::make('is_published')
-                    ->options([
-                        1 => 'True',
-                        0 => 'False',
-                    ])
-                    ->label('Publish')
-                    ->required(),
-                Forms\Components\DatePicker::make('published_at')
-                    ->required()
-                    ->label('Publish Date'),
-            ]);
-    }
+  public static function form(Form $form): Form
+  {
+    return $form
+      ->schema([
+        Forms\Components\TextInput::make('title')
+          ->required()
+          ->maxLength(255)
+          ->label('Title'),
+        Forms\Components\TextInput::make('content')
+          ->required()
+          ->maxLength(255),
+        Forms\Components\Radio::make('is_published')
+          ->options([
+            1 => 'True',
+            0 => 'False',
+          ])
+          ->label('Publish')
+          ->required(),
+        Forms\Components\DatePicker::make('published_at')
+          ->required()
+          ->label('Publish Date'),
+      ]);
+  }
 
-    public static function table(Table $table): Table
-    {
-        return $table
-            ->columns([
-                Tables\Columns\TextColumn::make('title')
-                    ->searchable()
-                    ->sortable(),
-                Tables\Columns\TextColumn::make('content'),
-                Tables\Columns\IconColumn::make('is_published')
-                    ->label('Status')
-                    ->boolean()
-                    ->color(fn (string $state): string => match ($state) {
-                        '0' => 'warning',
-                        '1' => 'success',
-                        default => 'gray',
-                    }),
-                Tables\Columns\TextColumn::make('published_at')
-                    ->dateTime()
-                    ->label('Published At'),
-            ])
-            ->actions([
-            Tables\Actions\EditAction::make(),
-            Tables\Actions\DeleteAction::make(),
-        ])
-            ->bulkActions([
-            Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
-            ]),
-        ]);
-    }
+  public static function table(Table $table): Table
+  {
+    return $table
+      ->columns([
+        Tables\Columns\TextColumn::make('title')
+          ->searchable()
+          ->label('Judul')
+          ->sortable(),
+        Tables\Columns\TextColumn::make('content')
+          ->label('Konten')
+        ,
+        Tables\Columns\IconColumn::make('is_published')
+          ->label('Status')
+          ->boolean()
+          ->color(fn(string $state): string => match ($state) {
+            '0' => 'warning',
+            '1' => 'success',
+            default => 'gray',
+          }),
+        Tables\Columns\TextColumn::make('published_at')
+          ->dateTime()
+          ->label('Tanggal Publish'),
+      ])
+      ->actions([
+        Tables\Actions\EditAction::make(),
+        Tables\Actions\DeleteAction::make(),
+      ])
+      ->bulkActions([
+        Tables\Actions\BulkActionGroup::make([
+          Tables\Actions\DeleteBulkAction::make(),
+        ]),
+      ]);
+  }
 
-    public static function getRelations(): array
-    {
-        return [];
-    }
+  public static function getRelations(): array
+  {
+    return [];
+  }
 
-    public static function getPages(): array
-    {
-        return [
-            'index' => Pages\ListAnnouncements::route('/'),
-            'create' => Pages\CreateAnnouncement::route('/create'),
-            'edit' => Pages\EditAnnouncement::route('/{record}/edit'),
-        ];
-    }
+  public static function getPages(): array
+  {
+    return [
+      'index' => Pages\ListAnnouncements::route('/'),
+      'create' => Pages\CreateAnnouncement::route('/create'),
+      'edit' => Pages\EditAnnouncement::route('/{record}/edit'),
+    ];
+  }
+
   public static function canViewAny(): bool
   {
     return Auth::check() && (Auth::user()->hasRole(RoleEnum::ADMIN->value) || Auth::user()->hasRole(RoleEnum::PETUGAS->value));
