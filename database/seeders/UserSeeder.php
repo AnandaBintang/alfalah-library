0a8f1d388965a373570ee8b4a182367e30617820
AnandaBintang <anandabintang4@gmail.com>
2025-08-20 23:32:36 +0700

    feat: default active for admin and petugas role on UserSeeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -10,70 +10,76 @@ use Spatie\Permission\Models\Role;
 
 class UserSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
-    {
-      if (config('app.env') === 'local') {
-        foreach (RoleEnum::cases() as $roleEnum) {
-          Role::firstOrCreate(['name' => $roleEnum->value]);
-        }
-        // Account siswa
-        $siswa = User::firstOrCreate(
-          ['email' => 'siswa@gmail.com'],
-          [
-            'name' => 'siswa',
-            'password' => Hash::make('password'),
-          ]
-        );
-        $siswa->assignRole(RoleEnum::SISWA->value);
-
-        // Account petugas
-        $petugas = User::firstOrCreate(
-          ['email' => 'petugas@gmail.com'],
-          [
-            'name' => 'petugas',
-            'password' => Hash::make('password'),
-          ]
-        );
-        $petugas->assignRole(RoleEnum::PETUGAS->value);
-
-        // Account admin
-        $admin = User::firstOrCreate(
-          ['email' => 'admin@gmail.com'],
-          [
-            'name' => 'admin',
-            'password' => Hash::make('password'),
-          ]
-        );
-        $admin->assignRole(RoleEnum::ADMIN->value);
-
-        // Akun admin
-        $perpustakaanAlfalah = User::firstOrCreate(
-          ['email' => 'perpustakaansmpalfalahassalam@gmail.com'],
-          [
-            'name' => 'Admin Perpustakaan SMP Alfalah',
-            'password' => Hash::make('password'),
-          ]
-        );
-
-        $perpustakaanAlfalah->assignRole(RoleEnum::ADMIN->value);
-
-
-
-        // User random factory
-        User::factory()
-          ->count(100)
-          ->create()
-          ->each(function ($user) {
-            $user->assignRole(RoleEnum::SISWA->value);
-          });
+  /**
+   * Run the database seeds.
+   */
+  public function run(): void
+  {
+    if (config('app.env') === 'local') {
+      foreach (RoleEnum::cases() as $roleEnum) {
+        Role::firstOrCreate(['name' => $roleEnum->value]);
       }
+      // Account siswa
+      $siswa = User::firstOrCreate(
+        ['email' => 'siswa@gmail.com'],
+        [
+          'name' => 'siswa',
+          'password' => Hash::make('password'),
+        ]
+      );
+      $siswa->assignRole(RoleEnum::SISWA->value);
 
-      if (config('app.env') === 'production') {
-        // Setup akun admin dengan email penjaga perpustakaan
-      }
+      // Account petugas
+      $petugas = User::firstOrCreate(
+        ['email' => 'petugas@gmail.com'],
+        [
+          'name' => 'petugas',
+          'password' => Hash::make('password'),
+          'is_active' => true,
+          'activated_at' => now(),
+          'expires_at' => now()->addYear(3),
+        ]
+      );
+      $petugas->assignRole(RoleEnum::PETUGAS->value);
 
+      // Account admin
+      $admin = User::firstOrCreate(
+        ['email' => 'admin@gmail.com'],
+        [
+          'name' => 'admin',
+          'password' => Hash::make('password'),
+          'is_active' => true,
+          'activated_at' => now(),
+          'expires_at' => now()->addYear(3),
+        ]
+      );
+      $admin->assignRole(RoleEnum::ADMIN->value);
+
+      // Akun admin
+      $perpustakaanAlfalah = User::firstOrCreate(
+        ['email' => 'perpustakaansmpalfalahassalam@gmail.com'],
+        [
+          'name' => 'Admin Perpustakaan SMP Alfalah',
+          'password' => Hash::make('password'),
+          'is_active' => true,
+          'activated_at' => now(),
+          'expires_at' => now()->addYear(3),
+        ]
+      );
+
+      $perpustakaanAlfalah->assignRole(RoleEnum::ADMIN->value);
+
+      // User random factory
+      User::factory()
+        ->count(100)
+        ->create()
+        ->each(function ($user) {
+          $user->assignRole(RoleEnum::SISWA->value);
+        });
     }
+
+    if (config('app.env') === 'production') {
+      // Setup akun admin dengan email penjaga perpustakaan
+    }
+  }
 }
